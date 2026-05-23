7f7e7294e492b085a0585c85bbea2fb32775ff50
Kamaleshwaran S <s.kamaleshwaran107@gmail.com>
2026-05-23 18:30:25 +0530

    Updated frontend and backend code

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -122,7 +122,7 @@ type UserRole = 'General User' | 'Admin';
                   <input
                     formControlName="userId"
                     type="email"
-                    placeholder="admin@mploychek.com"
+                    placeholder="admin@gmail.com"
                     class="w-full rounded-xl border border-slate-300 bg-white px-4 py-3 text-sm text-slate-900 outline-none transition focus:border-indigo-500 focus:ring-4 focus:ring-indigo-100"
                   />
 
